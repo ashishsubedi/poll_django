a0a851a9fe54801503e647232dd2f9ad6443cc13
Ashish Subedi <ashishsubedi10@gmail.com>
2020-12-06 12:57:53 +0545

    Completed 4th part

--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from django.shortcuts import render,get_object_or_404
+from .models import Question,Choice
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+
 from .models import Question
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-from .models import Question
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    context = {
-        'latest_question_list':latest_question_list
-    }
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-    return render(request,'polls/index.html',context)
-    
-
-def detail(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/detail.html',{'question':question})
-
-def results(request,question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response)
 
 def vote(request,question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question,id=question_id)
+    try:
+        print(request.POST['choice'])
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':"You didn't select a choice",
+        })
+    else:
+        # F() for avoiding race condition on database
+        selected_choice.votes = F('votes')+1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
